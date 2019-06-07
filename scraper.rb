e0f92005051fc0db6b49bd3c1a0794fa46970b56
Matthew Landauer <mlandauer@gmail.com>
2019-06-07 16:08:02 +1000

    Upgrade to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,3 +1,3 @@
 require "icon_scraper"
 
-IconScraper.rest_xml("https://elodge.swan.wa.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=thisweek&k=LodgementDate&t=282,281,283&o=xml")
+IconScraper.scrape_and_save(:swan)
